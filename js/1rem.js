171639a84ee32dd6675a7529fcff86ad468bab16
mafuqiang <mafuqiang@outlook.com>
2018-09-20 19:01:25 +0800

    fix link error add comments...

--- js/1rem.js
+++ js/1rem.js
@@ -1,32 +1,35 @@
-;+function(base, doc, win){ // base 
+// for simplified 750 divide into 100, no 10 for calculator simplified...
+//
+;+function (base, doc, win) { // base
 
-	var rootEl = doc.documentElement;
-	var metaEl = doc.querySelector('meta[name=viewport]');
-	var deviceRatio;
-	var pixelwidth = rootEl.clientWidth;
-	// confirm  test metaEl okay;
-	// or create one for set Attr;
-	if(!metaEl) {
-		metaEl = doc.createElement('meta');
-		metaEl.setAttribute('name', 'viewport');
-		doc.head.appendChild(metaEl);
-	}
+    var rootEl = doc.documentElement;
+    var metaEl = doc.querySelector('meta[name=viewport]');
+    var deviceRatio;
+    var pixelwidth = rootEl.clientWidth;
+    // confirm  test metaEl okay;
+    // or create one for set Attr;
+    if (!metaEl) {
+        metaEl = doc.createElement('meta');
+        metaEl.setAttribute('name', 'viewport');
+        doc.head.appendChild(metaEl);
+    }
 
-	 function ins(){
-		deviceRatio = win.devicePixelRatio;
+    function ins() {
+        deviceRatio = win.devicePixelRatio;
 
-		scale = 1 / deviceRatio;
-		// pixelwidth = rootEl.clientWidth;
-		pixelwidth = win.screen.width;
-		console.log(pixelwidth / base);
-		rootEl.style.fontSize = pixelwidth * deviceRatio / base + 'px';
-		console.log(rootEl.style.fontSize);
-		rootEl.dataset.dpr = deviceRatio;
-		metaEl.setAttribute('content', 'initial-scale=' + scale + ', maximum-scale=' + scale + ', minimum-scale=' + scale + ', user-scalable=no');
-		console.log(deviceRatio, pixelwidth);
-		console.log(scale);
-	}
-	ins();
-	win.onresize = ins;
+        scale = 1 / deviceRatio;
+        // pixelwidth = rootEl.clientWidth;
+        pixelwidth = win.screen.width;
+        console.log(pixelwidth / base);
+        rootEl.style.fontSize = pixelwidth * deviceRatio / base + 'px';
+        console.log(rootEl.style.fontSize);
+        rootEl.dataset.dpr = deviceRatio;
+        metaEl.setAttribute('content', 'initial-scale=' + scale + ', maximum-scale=' + scale + ', minimum-scale=' + scale + ', user-scalable=no');
+        console.log(deviceRatio, pixelwidth);
+        console.log(scale);
+    }
+
+    ins();
+    win.onresize = ins;
 
 }(750, document, window);
